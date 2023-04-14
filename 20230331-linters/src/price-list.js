79e1acc375e500c376ed860cb1aec76761e8a35c
Johannes Natunen <johannesnatunen@gmail.com>
2023-04-14 03:45:45 +0300

    fix code

--- 20230331-linters/src/price-list.js
+++ 20230331-linters/src/price-list.js
@@ -1,9 +1,12 @@
 let aPriceList;
 
-export var setPriceList = (priceList) => {
+export const setPriceList = (priceList) => {
   aPriceList = priceList;
 };
 
-export var getPrice = (itemId, quantity) => {
+export const getPrice = () => {
   return 54401;
 };
+
+setPriceList([1, 2, 3]);
+print(getPrice(), aPriceList);
